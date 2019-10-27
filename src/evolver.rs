9fbca49580f2ccd3d333b77bed168d8dabccca78
Felix Watts <felix@provenance.org>
2019-10-27 10:50:02 +0000

    clean up evolver

--- src/evolver.rs
+++ src/evolver.rs
@@ -33,34 +33,43 @@ impl<'a> Evolver<'a> {
         evolver
     }
 
-    fn spawn(&mut self, child: &mut Genome<'a>) {
-        let mother_id = self.rand.select_individual();
-        let father_id = self.rand.select_individual();
-        Genome::cross(&self.pop[mother_id], &self.pop[father_id], child, &mut self.rand);
-        child.mutate(&mut self.rand);
-    }
-
     pub fn step(&mut self) {
-
-        let mut next = vec!(crate::genome::Genome::new(&self.params); POPULATION_SIZE);
-
-        next[0] = self.get_best_solution().clone();
-
-        // build next generation by selection, crossover and mutation
-        for i in 1..POPULATION_SIZE {
-            self.spawn(&mut next[i]);
-        }
-
-        next.sort_by_cached_key(|p| p.to_phenome().score());
-
-        self.pop = next;
+        let mut next_population = self.get_new_population();
+        self.spawn_next_generation_into(&mut next_population);
+        self.sort_by_fitness(&mut next_population);
+        self.pop = next_population;
     }
-
-    // pub fn get_best_score(&self) -> i32 {
-    //     self.pop[POPULATION_SIZE-1].to_phenome().score()
-    // }
 
     pub fn get_best_solution(&self) -> &Genome<'a> {
         self.pop.last().unwrap()
+    }
+
+    fn spawn_next_generation_into(&mut self, next: &mut Vec<Genome<'a>>) {
+        // Elitism, best individual survives unchanged, means that best score can never decrease
+        next[0] = self.get_best_solution().clone();
+
+        for i in 1..POPULATION_SIZE {
+            self.spawn(&mut next[i]);
+        }
+    }
+
+    fn spawn(&mut self, child: &mut Genome<'a>) {
+        // Selection
+        let mother_id = self.rand.select_individual();
+        let father_id = self.rand.select_individual();
+
+        // Crossover
+        Genome::cross(&self.pop[mother_id], &self.pop[father_id], child, &mut self.rand);
+
+        // Mutation
+        child.mutate(&mut self.rand);
+    }
+
+    fn get_new_population(&self) -> Vec<Genome<'a>> {
+        vec!(crate::genome::Genome::new(&self.params); POPULATION_SIZE)
+    }
+
+    fn sort_by_fitness(&mut self, population: &mut Vec<Genome<'a>>) {
+        population.sort_by_cached_key(|p| p.to_phenome().score());
     }
 }
