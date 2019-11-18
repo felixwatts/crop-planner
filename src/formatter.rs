762a488be881b13d349dc4b681ffa406c64570de
Felix Watts <felix@provenance.org>
2019-11-18 12:25:19 +0000

    Plan refactor (#21)
    * refactor: introduce Plan type
    
    * rename satisfaction to saturation and print value if over 100%

--- src/formatter.rs
+++ src/formatter.rs
@@ -38,8 +38,8 @@ impl<'a> Display for Formatter<'a> {
                 match self.params.varieties[v].is_harvestable_in_week(week) {
                     true => {
                         let harvestable_units = harvest_plan[v][week];
-                        let satisfaction = std::cmp::min(harvestable_units, self.params.num_baskets) as f32 / self.params.num_baskets as f32;
-                        write!(f, "{:>10.0}%", satisfaction * 100.0)?;
+                        let saturation = harvestable_units as f32  / self.params.num_baskets as f32;
+                        write!(f, "{:>10.0}%", saturation * 100.0)?;
                     },
                     false => {
                         write!(f, "           ")?;
@@ -52,7 +52,7 @@ impl<'a> Display for Formatter<'a> {
 
         writeln!(f)?;
         writeln!(f, "Utilization: {:.0}%", evaluator.get_bed_utilization() * 100.0)?;
-        writeln!(f, "Satisfaction: {:.0}%", evaluator.get_basket_satisfaction() * 100.0)?;
+        writeln!(f, "Saturation: {:.0}%", evaluator.get_market_saturation() * 100.0)?;
         writeln!(f, "Profit: {:.2}", evaluator.get_profit() as f32 / 100.0)
     }
 }
